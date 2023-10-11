9b2b2b26ef5a8a0c47120fcbec59a50c50edaefe
SvenKeimpema <keimpje1234@gmail.com>
2023-10-11 14:38:09 +0200

    small fix for code blocks

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ tokens = lexer.lex_file('test/test.html')
 parser = DefaultParser(tokens)
 program = parser.parse()
 renderer = DefaultRenderer()
-print(renderer.render(program))
+# use spacing in this one
+code = renderer.generate_pre_code(program)
+print(renderer.render_pre(code, test=3))
