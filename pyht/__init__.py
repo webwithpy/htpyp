24f2412daa0c961c9109d4262a5c2abbc8ff9f83
SvenKeimpema <keimpje1234@gmail.com>
2023-11-01 14:53:39 +0100

    rename

--- pyht/__init__.py
+++
@@ -1,3 +0,0 @@
-from .lexer import Lexer
-from .parser import DefaultParser
-from .renderer import DefaultRenderer
--- pyht/helpers/str_helper.py
+++
@@ -1,2 +0,0 @@
-def remove_quotes(html_input: str) -> str:
-    return html_input.replace('\'', '').replace('\"', '')
